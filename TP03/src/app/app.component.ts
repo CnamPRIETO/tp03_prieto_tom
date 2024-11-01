98c79b7f01321da909f21ce2836cdf5bf2664cfc
Tom PRIETO <tom.prieto@etu.unistra.fr>
2024-11-01 22:34:02 +0100

    fin

--- TP03/src/app/app.component.ts
+++ TP03/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CatalogueComponent } from './catalogue/catalogue.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CatalogueComponent],
+  imports: [RouterOutlet, CatalogueComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
